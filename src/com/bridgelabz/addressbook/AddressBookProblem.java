a5977300dcf5e676b6187798f2923bb29b9191df
shitalfating <shitalfating84@gmail.com>
2023-01-19 15:50:06 +0530

    Added:Welcome to Address Book Program

---
+++ src/com/bridgelabz/addressbook/AddressBookProblem.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.addressbook;
+
+public class AddressBookProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+    }
+}
