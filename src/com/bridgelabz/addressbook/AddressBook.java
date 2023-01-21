a342cf4fd597b7fb7efd6bcb8811609e1007f180
shitalfating <shitalfating84@gmail.com>
2023-01-21 17:32:34 +0530

    Refactored:Deleting the Existing Contact

--- src/com/bridgelabz/addressbook/AddressBook.java
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -45,4 +45,24 @@ public class AddressBook {
         System.out.println("Enter email");
         String email = scanner.next();
     }
+
+    public void deleteContact() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter First Name");
+        String name = scanner.next();
+        System.out.println("Enter last Name");
+        String surname = scanner.next();
+        System.out.println("Enter address");
+        String address = scanner.next();
+        System.out.println("Enter city");
+        String city = scanner.next();
+        System.out.println("Enter state");
+        String state = scanner.next();
+        System.out.println("Enter zip");
+        String zip = scanner.next();
+        System.out.println("Enter phone number");
+        String phone = scanner.next();
+        System.out.println("Enter email");
+        String email = scanner.next();
+    }
 }
