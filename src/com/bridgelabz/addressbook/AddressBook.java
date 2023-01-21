8326917d83861ba75b2fe89ce730e58bea29b701
shitalfating <shitalfating84@gmail.com>
2023-01-21 17:23:06 +0530

    Refactored:Edit Existing Contact

--- src/com/bridgelabz/addressbook/AddressBook.java
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -6,24 +6,43 @@ public class AddressBook {
 
     Contact contact = new Contact();
 
-     void addContact(){
-         Scanner scanner = new Scanner(System.in);
-         System.out.println("Enter First Name");
-         String name = scanner.next();
-         System.out.println("Enter last Name");
-         String surname = scanner.next();
-         System.out.println("Enter address");
-         String address = scanner.next();
-         System.out.println("Enter city");
-         String city = scanner.next();
-         System.out.println("Enter state");
-         String state = scanner.next();
-         System.out.println("Enter zip");
-         String zip = scanner.next();
-         System.out.println("Enter phone number");
-         String phone = scanner.next();
-         System.out.println("Enter email");
-         String email = scanner.next();
-     }
-}
+    void addContact() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter First Name");
+        String name = scanner.next();
+        System.out.println("Enter last Name");
+        String surname = scanner.next();
+        System.out.println("Enter address");
+        String address = scanner.next();
+        System.out.println("Enter city");
+        String city = scanner.next();
+        System.out.println("Enter state");
+        String state = scanner.next();
+        System.out.println("Enter zip");
+        String zip = scanner.next();
+        System.out.println("Enter phone number");
+        String phone = scanner.next();
+        System.out.println("Enter email");
+        String email = scanner.next();
+    }
 
+    public void editContact() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter First Name");
+        String name = scanner.next();
+        System.out.println("Enter last Name");
+        String surname = scanner.next();
+        System.out.println("Enter address");
+        String address = scanner.next();
+        System.out.println("Enter city");
+        String city = scanner.next();
+        System.out.println("Enter state");
+        String state = scanner.next();
+        System.out.println("Enter zip");
+        String zip = scanner.next();
+        System.out.println("Enter phone number");
+        String phone = scanner.next();
+        System.out.println("Enter email");
+        String email = scanner.next();
+    }
+}
