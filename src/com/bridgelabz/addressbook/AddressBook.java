2d667cb227b50c1d5bb075c79578326f3854640a
shitalfating <shitalfating84@gmail.com>
2023-02-12 15:38:57 +0530

    Refactored:Adding Multiple contact

--- src/com/bridgelabz/addressbook/AddressBook.java
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -1,10 +1,16 @@
 package com.bridgelabz.addressbook;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
 
     Contact contact = new Contact();
+    ArrayList<Contact> List = new ArrayList<>();
+
+    public void setContact(Contact contact) {
+        this.contact = contact;
+    }
 
     void addContact() {
         Scanner scanner = new Scanner(System.in);
@@ -24,9 +30,19 @@ public class AddressBook {
         String phone = scanner.next();
         System.out.println("Enter email");
         String email = scanner.next();
+        contact.setName(name);
+        contact.setSurname(surname);
+        contact.setAddress(address);
+        contact.setCity(city);
+        contact.setState(state);
+        contact.setZip(zip);
+        contact.setPhone(phone);
+        contact.setEmail(email);
+
     }
 
     public void editContact() {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter First Name");
         String name = scanner.next();
@@ -44,6 +60,14 @@ public class AddressBook {
         String phone = scanner.next();
         System.out.println("Enter email");
         String email = scanner.next();
+        contact.setName(name);
+        contact.setSurname(surname);
+        contact.setAddress(address);
+        contact.setCity(city);
+        contact.setState(state);
+        contact.setZip(zip);
+        contact.setPhone(phone);
+        contact.setEmail(email);
     }
 
     public void deleteContact() {
@@ -64,5 +88,27 @@ public class AddressBook {
         String phone = scanner.next();
         System.out.println("Enter email");
         String email = scanner.next();
+
+//        public void addContact(){
+//            scanner = new Scanner(System.in);
+//            System.out.println("Enter First Name");
+//            name = scanner.next();
+//            System.out.println("Enter last Name");
+//            surname = scanner.next();
+//            System.out.println("Enter address");
+//            address = scanner.next();
+//            System.out.println("Enter city");
+//            city = scanner.next();
+//            System.out.println("Enter state");
+//            state = scanner.next();
+//            System.out.println("Enter zip");
+//            zip = scanner.next();
+//            System.out.println("Enter phone number");
+//            phone = scanner.next();
+//            System.out.println("Enter email");
+//            email = scanner.next();
+//            Contact contactList = new Contact();
+//            List.add( contactList);
+//        }
     }
 }
